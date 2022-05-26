cdad73c16a1b320f3166208307cce125e6e037e6
Tejas <tejasjdorge@gmail.com>
2022-05-26 14:52:49 +0530

    initial commit

---
+++ src/core/settings/production.py
@@ -0,0 +1,24 @@
+
+
+DEBUG = False
+
+
+DATABASES = {
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'dd9tbprtlt5a5p',
+
+        'USER': 'ldupkzxagudcnh',
+
+        'PASSWORD': '8517d1a91aabcad0aa8f13baed990203d47b8bf99a49555b448815417d0c3a99',
+
+        'HOST': 'ec2-52-44-209-165.compute-1.amazonaws.com',
+
+        'PORT': '5432',
+
+    }
+
+}
