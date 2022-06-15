d9b26df2872280cd02086af85663127ba3bd365c
Tejas <tejasjdorge@gmail.com>
2022-06-15 23:21:06 +0530

    make it better

--- src/manage.py
+++ src/manage.py
@@ -2,13 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from django.core.management import execute_from_command_line
+
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
     try:
-        execute_from_command_line
+        from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
