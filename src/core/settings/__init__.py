580f49e5d131766d17a29e595e2c9ecd0042c4ba
Tejas <tejasjdorge@gmail.com>
2022-06-16 09:27:43 +0530

    make it better

--- src/core/settings/__init__.py
+++ src/core/settings/__init__.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ['localhost','src.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','core.herokuapp.com']
 
 DEBUG = True
 # Application definition
