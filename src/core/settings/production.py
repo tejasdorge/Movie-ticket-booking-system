57e134ec74dc0345ca4ebe7d86e851c8ec919ddc
Tejas <tejasjdorge@gmail.com>
2022-06-19 20:49:43 +0530

    make it better

--- src/core/settings/production.py
+++ src/core/settings/production.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import os
-# from decouple import config # To Retrieve the configuration parameters (from heroku)
+from decouple import config # To Retrieve the configuration parameters (from heroku)
 import django_heroku
 
 # SECRET_KEY = config('SECRET_KEY')
@@ -30,50 +30,32 @@ DATABASES = {
 
 }
 
-# AWS SETTINGS
 
-# AWS_ACCESS_KEY_ID = 'AKIASS227E3VTXHNGYV2'
-# AWS_SECRET_ACCESS_KEY = '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'
-# AWS_STORAGE_BUCKET_NAME = 'django-demo123'
-# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-# AWS_S3_OBJECT_PARAMETERS = {
-#     'CacheControl': 'max-age=86400',
-# }
-# AWS_DEFAULT_ACL = 'public-read'
-# AWS_LOCATION = 'static'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
-# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# DEFAULT_FILE_STORAGE = 'core.storages.MediaStore'
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 
-# AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 
-# AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-# AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_DEFAULT_ACL = 'public-read'
 
-# AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
-# AWS_DEFAULT_ACL = 'public-read'
+AWS_LOCATION = 'static'
 
-# AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_QUERYSTRING_AUTH = False
 
-# AWS_LOCATION = 'static'
+AWS_HEADERS = {'Access-Control-Allow-Origin': '*'}
 
-# AWS_QUERYSTRING_AUTH = False
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# AWS_HEADERS = {'Access-Control-Allow-Origin': '*'}
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
-
-# STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-
-# MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 
