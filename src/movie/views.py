209f8a388ceaf42337cfd076d70a31da12b150aa
Tejas <tejasjdorge@gmail.com>
2022-06-19 22:39:51 +0530

    make it better

--- src/movie/views.py
+++ src/movie/views.py
@@ -24,7 +24,6 @@ class MovieViewSet(viewsets.ModelViewSet):
     permission_classes = (MyPermission,)
     serializer_class = MovieSerializer
     queryset = Movie.objects.all()
-    print(queryset)
     filter_backends = [SearchFilter,OrderingFilter]
     search_fields = ['title','cast__name']
     ordering_fields = ['popularity_index']
