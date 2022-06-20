e663f415ea4f6d445cda8fc7056f20189dc89a93
Tejas <tejasjdorge@gmail.com>
2022-06-20 15:13:27 +0530

    make it better

--- src/core/storage_backends.py
+++ src/core/storage_backends.py
@@ -2,7 +2,9 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class MediaStore(S3Boto3Storage):
-    location = 'media/'
+    location = 'media'
     file_overwrite = False
 
-#S3Boto3Storage to add a few custom parameters, in order to be able to store the user uploaded files, that is, the media assets in a different location and also to tell S3 to not override files with the same name
+
+# S3Boto3Storage to add a few custom parameters, in order to be able to store the user uploaded files,
+# that is, the media assets in a different location and also to tell S3 to not override files with the same name
