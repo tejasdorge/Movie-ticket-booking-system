64a08ec134be2d22217ed011aace629c81537292
Tejas <tejasjdorge@gmail.com>
2022-06-20 13:45:56 +0530

    make it better

--- src/core/storages.py
+++ src/core/storages.py
@@ -1,6 +1,6 @@
-# from storages.backends.s3boto3 import S3Boto3Storage
+from storages.backends.s3boto3 import S3Boto3Storage
 
 
-# class MediaStore(S3Boto3Storage):
-#     location = 'media'
-#     file_overwrite = False
+class MediaStore(S3Boto3Storage):
+    location = 'media'
+    file_overwrite = False
