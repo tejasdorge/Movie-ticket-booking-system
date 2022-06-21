603fd6d4bd7f29de9218f49b1534cbff0c110e28
Tejas <tejasjdorge@gmail.com>
2022-06-21 23:15:49 +0530

    make it better

--- src/core/settings/__init__.py
+++ src/core/settings/__init__.py
@@ -68,9 +68,7 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:8000",
-]
+CORS_ALLOW_ALL_ORIGINS:True
 
 LOGGING = {
     'version': 1,
